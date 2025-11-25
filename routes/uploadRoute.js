d74767d049e4cd22c264ab75c52f6fc8127886a3
Nilavalakan A <nilavalakan002@gmail.com>
2025-11-25 21:56:16 +0530

    switched to cloudinary uploads

--- routes/uploadRoute.js
+++ routes/uploadRoute.js
@@ -1,18 +1,40 @@
 const express = require("express");
-const { uploadImages, deleteImages } = require("../controller/uploadCtrl");
 const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
-const { uploadPhoto, productImgResize } = require("../middlewares/uploadImage");
 const router = express.Router();
+
+const cloudinary = require("../config/cloudinary");
+const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "products",
+    allowed_formats: ["jpg", "jpeg", "png", "webp"],
+  },
+});
+
+const upload = multer({ storage });
 
 router.post(
   "/",
   authMiddleware,
   isAdmin,
-  uploadPhoto.array("images", 10),
-  productImgResize,
-  uploadImages
+  upload.array("images", 10),
+  (req, res) => {
+    const urls = req.files.map((file) => file.path);
+    res.json({ urls });
+  }
 );
 
-router.delete("/delete-img/:id", authMiddleware, isAdmin, deleteImages);
+router.delete("/delete-img/:id", authMiddleware, isAdmin, async (req, res) => {
+  try {
+    const public_id = req.params.id;
+    await cloudinary.uploader.destroy(public_id);
+    res.json({ message: "Deleted" });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
