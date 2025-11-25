a1b53d848a526a260d6e13c1386416ec4bc524f7
Nilavalakan A <nilavalakan002@gmail.com>
2025-11-25 23:14:16 +0530

    Fixed img problem

---
+++ controller/uploadCtrl.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const asyncHandler = require("express-async-handler");
+
+const {
+  cloudinaryUploadImg,
+  cloudinaryDeleteImg,
+} = require("../utils/cloudinary");
+
+const uploadImages = asyncHandler(async (req, res) => {
+  try {
+    const uploader = (path) => cloudinaryUploadImg(path, "images");
+    const urls = [];
+    const files = req.files;
+
+    for (const file of files) {
+      const { path } = file;
+      const newpath = await uploader(path);
+
+      urls.push({
+        url: newpath.secure_url,
+        public_id: newpath.public_id,
+      });
+
+      fs.unlinkSync(path);
+    }
+
+    res.json(urls);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const deleteImages = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    await cloudinaryDeleteImg(id, "images");
+    res.json({ message: "Deleted" });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = {
+  uploadImages,
+  deleteImages,
+};
